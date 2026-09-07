4f22746ea7fac65cc42217758a21061bf7147ad9
Aixa Mengassini <verenice.aixa@gmail.com>
2026-09-07 16:29:10 -0300

    feat: casos de uso de UnidadDeVenta, consultas de Pedido/Plato y ajustes en Dao

--- src/dao/PersonalDao.java
+++ src/dao/PersonalDao.java
@@ -2,7 +2,6 @@ package dao;
 import java.util.List;
 
 import datos.Personal;
-import datos.Cajero;
 
 public class PersonalDao extends Dao<Personal> {
     private static PersonalDao instancia = null;
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,12 +1,17 @@
 package test;
+
 import java.time.LocalDate;
+import java.util.List;
 
 import org.hibernate.Session;
 import dao.HibernateUtil;
 import negocio.PedidoABM;
 import negocio.PlatoABM;
+import negocio.UnidadDeVentaABM;
 import datos.Pedido;
 import datos.Plato;
+import datos.PuestoDesarmable;
+import datos.UnidadDeVenta;
 
 public class Test {
 
@@ -16,10 +21,8 @@ public class Test {
 		session.close();
 		System.out.println("OK");
 		
-		
 		try {
 			System.out.println("\n=== INICIO DE PRUEBAS PEDIDO Y PLATO ===");
-
 
 			Pedido pedidoGuardado = PedidoABM.getInstancia().traer(1);
 			System.out.println("Pedido recuperado de BD: " + pedidoGuardado);
@@ -30,13 +33,26 @@ public class Test {
 				System.out.println(" - " + p.getNombre() + " ($" + p.getPrecioVenta() + ")");
 			}
 
+			System.out.println("\n=== CASO DE USO 1: Unidades por Festival con Personal ===");
+			int idFestival = 1;
+			List<UnidadDeVenta> unidades = UnidadDeVentaABM.getInstancia().traerUnidadesPorFestival(idFestival);
+			for (UnidadDeVenta uv : unidades) {
+				int cantPersonal = (uv.getPersonal() != null) ? uv.getPersonal().size() : 0;
+				System.out.println("Unidad: " + uv.getNombreComercial() + " | Personal: " + cantPersonal);
+			}
+
+			System.out.println("\n=== CASO DE USO 2: Puestos con tiempo de montaje superior a limite ===");
+			int tiempoLimite = 30;
+			List<PuestoDesarmable> puestos = UnidadDeVentaABM.getInstancia().traerPuestosConTiempoMontajeMayorA(tiempoLimite);
+			for (PuestoDesarmable p : puestos) {
+				System.out.println("Puesto: " + p.getNombreComercial() + " | Tiempo: " + p.getTiempoMontaje() + " min");
+			}
+
 			System.out.println("\n=== PRUEBAS FINALIZADAS CON ÉXITO ===");
 
 		} catch (Exception e) {
 			System.err.println("Error durante las pruebas de la capa de negocio:");
 			e.printStackTrace();
 		}
-		
 	}
-
 }
